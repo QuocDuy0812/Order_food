91d0247d287c7c9e815d6be95e431ea24a64172e
QuocDuy0812 <duy079201024712@gmail.com>
2024-06-21 18:19:04 +0700

    Initial commit

---
+++ order_food-main/lib/ui/shared/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AColor {
+  static Color primary = const Color(0xffe5fff9);
+  static Color yellow = Colors.yellow;
+  static Color greenBold = const Color(0xff12c935);
+  static Color textGreen = const Color(0xff844f96);
+  static Color textWhite = const Color(0xffd8fff3);
+  static Color black = const Color(0xff645913);
+  static Color white = const Color(0xfff6fff5);
+  static Color grey = Colors.grey;
+  static Color cardColor = const Color(0xffd3fcf2);
+  static Color red = const Color(0xffFF0000);
+}
